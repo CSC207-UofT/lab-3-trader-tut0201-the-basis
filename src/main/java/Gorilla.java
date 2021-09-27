45708c8ebb75bbf03ddc8a3140058bcee02a4acc
Nofel Yazdani <nofel.yazdani@mail.utoronto.ca>
2021-09-27 13:54:31 -0400

    gorilla commit

--- src/main/java/Gorilla.java
+++ src/main/java/Gorilla.java
@@ -1,2 +1,48 @@
-public class Gorilla {
+import java.util.Objects;
+
+public class Gorilla implements Drivable, Domesticatable, Tradable {
+    private int maxSpeed;
+
+    public Gorilla() {
+        this.maxSpeed = 10;
+    }
+
+    @Override
+    public String sound() {
+        return "meowwww!";
+    }
+
+    @Override
+    public void upgradeSpeed() {
+        this.maxSpeed++;
+    }
+
+    @Override
+    public void downgradeSpeed() {
+        this.maxSpeed--;
+    }
+
+    @Override
+    public void getMaxSpeed() {
+        return this.maxSpeed;
+    }
+
+    @Override
+    public String toString(){
+        String id = super.toString();
+        return id + " (Gorrilla man speed: " + this.maxSpeed +")";
+    }
+    @Override
+    public int getPrice() {
+        return 10000000000000000000000000;
+    }
+    public String similarity(String person) {
+        String k = "";
+        if (Objects.equals(person, "black")){
+            k = "yes" + person + " people are similarity to Gorillas"
+        }
+        return k;
+
+
+    }
 }
