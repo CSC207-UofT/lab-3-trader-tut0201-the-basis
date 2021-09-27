1cf03f3a1aa87f330408f2ecdf4e707ed09e00ed
Vaishnav <vaishnav.kunhiraman@mail.utoronto.ca>
2021-09-27 13:29:45 -0400

    l

---
+++ src/main/java/Panda.java
@@ -0,0 +1,27 @@
+public class Panda implements Drivable, Domesticatable, Tradable{
+
+    @Override
+    public String sound() {
+        return null;
+    }
+
+    @Override
+    public void upgradeSpeed() {
+
+    }
+
+    @Override
+    public void downgradeSpeed() {
+
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return 1;
+    }
+
+    @Override
+    public int getPrice() {
+        return 1000;
+    }
+}
