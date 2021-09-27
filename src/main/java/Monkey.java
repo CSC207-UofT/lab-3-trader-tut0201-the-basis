72bcd8b1679f1921f4eced9f93a31950ddf07bd8
HamzaShariff <86576768+HamzaShariff@users.noreply.github.com>
2021-09-27 13:41:18 -0400

    hehe

--- src/main/java/Monkey.java
+++ src/main/java/Monkey.java
@@ -1,12 +1,12 @@
 public class Monkey implements Domesticatable, Tradable {
 
     @Override
-    public String sound() {
+    private String sound() {
         return "OOh OOh Ahh Ahh";
     }
 
     @Override
     public int getPrice() {
-        return 100.5;
+        return 100;
     }
 }
