b1bbf93f1a145d0ae6797c9d293f7a3a1113dc77
krhobeck <37283880+krhobeck@users.noreply.github.com>
2021-09-27 13:27:25 -0400

    made donkey 1

--- src/main/java/Donkey.java
+++ src/main/java/Donkey.java
@@ -1,26 +1,34 @@
 public class Donkey implements Drivable, Domesticatable, Tradable{
+    private int maxSpeed;
+
     @Override
     public String sound() {
-        return null;
+        return "Neighhh";
     }
 
     @Override
     public void upgradeSpeed() {
-
+        this.maxSpeed++;
     }
 
     @Override
     public void downgradeSpeed() {
-
+        this.maxSpeed--;
     }
 
     @Override
     public int getMaxSpeed() {
-        return 0;
+        return this.maxSpeed;
     }
 
     @Override
     public int getPrice() {
-        return 0;
+        return 20;
+    }
+
+    @Override
+    public String toString(){
+        String id = super.toString();
+        return id + " (Max speed: " + this.maxSpeed +")";
     }
 }
